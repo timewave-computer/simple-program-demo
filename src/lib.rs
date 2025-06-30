3d04787a03c2bd61deba272311fdd0bd7b38da1f
Parthiv Seetharaman <parthivs@myrdd.info>
2025-06-30 08:07:20 -0700

    update addresses

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use alloy::primitives::{address, Address};
 pub const SP1_VERIFIER: &str = "0x397A5f7f3dBd538f23DE225B51f532c34448dA9B";
-pub const AUTHORIZATION: Address = address!("0x84F36aeF81aBf1E34bcA9e470fE15e12697CB7Fd");
-pub const FORWARDER : Address = address!("0x7811A1648e43F1bC207d7DF21B039AE9D2870b91");
-pub const COPROCESSOR_APP_ID : &str = "2b991f1c74d0b0a03c490d35fcf8ce8233247a3c6ee8ad5561179796145d6362";
+pub const FORWARDER : Address = address!("0xcF8f8313C587c6Ec6E49Be286942D451D4E0908A");
+pub const AUTHORIZATION: Address = address!("0x3f75c48fceefAb5A28649e360288a4a29262bea6");
+pub const COPROCESSOR_APP_ID : &str = "2504124b075de5b94b8b3af8404849f5fcb3ecc35edaba8960c2372e1cc24480";
 pub mod types;
